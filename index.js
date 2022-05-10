1dabdf0f91305b0d1a875f1dd47684751ad48812
GabrielXcabral <95001693+GabrielXcabral@users.noreply.github.com>
2022-05-10 00:54:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 const limparFormulario = (endereco) =>{
     document.getElementById('endereco').value = '';
     document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
+    document.getElementById('cidade').value = '';
 }
 
 
 const preencherFormulario = (endereco) =>{
     document.getElementById('endereco').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
     document.getElementById('estado').value = endereco.uf;
+    document.getElementById('cidade').value = endereco.localidade;
 }
 
 
